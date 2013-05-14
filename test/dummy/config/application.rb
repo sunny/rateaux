126c68fc0a7e0902c1e104aa7a7e8378ed570476
Sunny Ripert <sunny@sunfox.org>
2013-05-14 19:25:44 +0200

    Load active record after all

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,11 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-# require 'rails/all'
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "rails/test_unit/railtie"
-require "sprockets/railtie"
+require 'rails/all'
 
 Bundler.require(*Rails.groups)
 require "rateaux"
@@ -52,7 +47,7 @@ module Dummy
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    # config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
